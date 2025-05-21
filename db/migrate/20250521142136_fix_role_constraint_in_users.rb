9c71c8b428351819d329b4f1a20124d9134009d7
s20211435 <yotamoti415717@gmail.com>
2025-05-22 00:51:11 +0900

    マイグレーションファイルの設定

--- db/migrate/20250521142136_fix_role_constraint_in_users.rb
+++ db/migrate/20250521142136_fix_role_constraint_in_users.rb
@@ -1,13 +1,13 @@
 class FixRoleConstraintInUsers < ActiveRecord::Migration[8.0]
   def up
-    # 既存の制約を削除（エラーを無視）
+    # 既存の制約を削除
     execute <<-SQL
       ALTER TABLE users DROP CONSTRAINT IF EXISTS role_check;
     SQL
 
     # 新しい制約を追加
     execute <<-SQL
-      ALTER TABLE users ADD CONSTRAINT role_check CHECK (role IN ('superuser', 'user', 'admin'));
+      ALTER TABLE users ADD CONSTRAINT role_check CHECK (role IN ("superuser", "user", "admin"));
     SQL
   end
 
@@ -19,7 +19,7 @@ class FixRoleConstraintInUsers < ActiveRecord::Migration[8.0]
 
     # 元の制約を再追加
     execute <<-SQL
-      ALTER TABLE users ADD CONSTRAINT role_check CHECK (role IN ('superuser', 'user'));
+      ALTER TABLE users ADD CONSTRAINT role_check CHECK (role IN ("superuser", "user"));
     SQL
   end
 end
