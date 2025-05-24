7141130fd557019dec6f0bcd399c384c9ac6e517
s20211435 <yotamoti415717@gmail.com>
2025-05-24 22:04:22 +0900

    パスの扱いを開発環境と本番環境の両方で動作するように修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  # パス文字列からURLを解決するヘルパーメソッド
+  def resolve_path(path_string)
+    if path_string.start_with?('/')
+      path_string  # 既に実際のパス
+    else
+      begin
+        send(path_string)  # ヘルパーメソッド名
+      rescue NoMethodError
+        '/'  # エラー時はホームにリダイレクト
+      end
+    end
+  end
 end
